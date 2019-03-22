dfb993666268a5367e08579a1101fd074faa4036
Herald Yu <yuhr123@gmail.com>
2019-03-22 11:38:16 +0800

    添加新的 add 功能

--- tongbu_cli.py
+++ tongbu_cli.py
@@ -7,7 +7,7 @@ def cli():
 
 @cli.command()
 def list():
-    chains = ['INPUT', 'OUTPUT']
+    chains = ['INPUT', 'OUTPUT', 'TRAFFIC_QUOTA']
 
     while True:
         if click.confirm('开始查看链上的记录？'):
@@ -57,9 +57,61 @@ def delport():
             click.echo('Bye!')
             break
 
-# @cli.command()
-# def add():
-#     subprocess.run()
+@cli.command()
+def add():
+    click.echo('功能初始化...')
+    cmd_traffic_chain = 'sudo iptables -N TRAFFIC_QUOTA'
+    add_traffic_chain = subprocess.run(cmd_traffic_chain.split(), capture_output=True)
+    if add_traffic_chain.returncode:
+        click.echo('已存在 TRAFFIC_QUOTA 链 [跳过]')
+    else:
+        click.echo('创建 TRAFFIC_QUOTA 链 [完成]')
+        # 添加流量限额规则, 50GB
+        cmd_quota_1 = 'sudo iptables -A TRAFFIC_QUOTA -m quota --quota 50000000000 -j ACCEPT'
+        cmd_quota_2 = 'sudo iptables -A TRAFFIC_QUOTA -j DROP'
+        add_quota_accept = subprocess.run(cmd_quota_1.split(), capture_output=True)
+        if add_quota_accept.returncode:
+            click.echo(add_quota_accept.stderr)
+        else:
+            click.echo('创建 流量限额接受规则 [完成]')
+        add_auota_drop = subprocess.run(cmd_quota_2.split(), capture_output=True)
+        if add_auota_drop.returncode:
+            click.echo(add_auota_drop.stderr)
+        else:
+            click.echo('创建 流量限额拒绝规则 [完成]')
+        click.echo('初始化完成！')
+
+    while True:
+        if click.confirm('是否创新的新的防火墙规则？'):
+            # TODO 输入端口规则验证
+            ports = click.prompt('请输入要开放的端口号，用逗号分隔多个端口，用冒号设置端口段')
+            cmd_1 = 'sudo iptables -I INPUT -p tcp -m multiport --sports %s -g TRAFFIC_QUOTA' % ports
+            cmd_2 = 'sudo iptables -I OUTPUT -p tcp -m multiport --sports %s -g TRAFFIC_QUOTA' % ports
+            add_ports_input = subprocess.run(cmd_1.split(), capture_output=True)
+            if add_ports_input.returncode:
+                click.echo('创建指定端口的入站规则 [失败]')
+                click.echo(add_ports_input.stderr)
+                break
+            else:
+                click.echo('创建指定端口的入站规则 [完成]')
+
+            add_ports_output = subprocess.run(cmd_2.split(), capture_output=True)
+            if add_ports_output.returncode:
+                click.echo('创建指定端口的出站规则 [失败]')
+                click.echo(add_ports_output.stderr)
+                break
+            else:
+                click.echo('创建指定端口的出站规则 [完成]')
+
+            # 打印规则列表
+            chains = ['INPUT', 'OUTPUT', 'TRAFFIC_QUOTA']
+
+            for c in chains:
+                rules = 'sudo iptables -L %s -nv' % c
+                subprocess.run(rules.split())
+                
+        else:
+            break
 
 if __name__ == '__main__':
     cli()
