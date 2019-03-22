727c24db6c4bcc076c012f7a1b24c6c17d3ea983
Herald Yu <yuhr123@gmail.com>
2019-03-22 17:01:09 +0800

    为每条规则设置独立的流量配额链

--- tongbu_cli.py
+++ tongbu_cli.py
@@ -13,24 +13,19 @@ def cli():
 @cli.command()
 def list():
     """查看指定链上的规则"""
-    chains = [
-        'INPUT',
-        'OUTPUT',
-        'TRAFFIC_QUOTA_10GB',
-        'TRAFFIC_QUOTA_30GB',
-        'TRAFFIC_QUOTA_60GB',
-        'TRAFFIC_QUOTA_100GB',
-    ]
+    chains = ['ALL', 'INPUT', 'OUTPUT']
 
     while True:
         if click.confirm('开始查看链上的记录？'):
             for n, c in enumerate(chains):
                 entry = str(n) + ' - ' + c
                 click.echo(entry)
-            select_chain = click.prompt('请选择要查看的链编号', default=1, type=int)
-            cmd = ['sudo', 'iptables', '-L',
-                   chains[select_chain], '-nv', '--line-number']
-            subprocess.run(cmd)
+            select_chain = click.prompt('请选择要查看的链编号', default=0, type=int)
+            if select_chain == 0:
+                cmd = 'sudo iptables -L -nv --line-number'
+            else:
+                cmd = 'sudo iptables -L %s -nv --line-number' % chains[select_chain]
+            subprocess.run(cmd.split())
         else:
             break
 
@@ -38,54 +33,45 @@ def list():
 @cli.command()
 def addrule():
     """添加开放端口规则"""
-    click.echo('功能初始化...')
-    quota_chains = {
-        'TRAFFIC_QUOTA_10GB': 10000000000,
-        'TRAFFIC_QUOTA_30GB': 30000000000,
-        'TRAFFIC_QUOTA_60GB': 60000000000,
-        'TRAFFIC_QUOTA_100GB': 100000000000,
-    }
-    for k, v in quota_chains.items():
-        cmd = 'sudo iptables -N %s' % k
-        add_traffic_chain = subprocess.run(cmd.split(), capture_output=True)
-
-        if add_traffic_chain.returncode:
-            click.echo('已存在 %s 链 [跳过]' % k)
-        else:
-            click.echo('创建 %s 链 [完成]' % k)
-            cmd_quota_1 = 'sudo iptables -A %s -m quota --quota %s -j ACCEPT' % (
-                k, str(v))
-            cmd_quota_2 = 'sudo iptables -A %s -j DROP' % k
-            add_quota_accept = subprocess.run(
-                cmd_quota_1.split(), capture_output=True)
-            if add_quota_accept.returncode:
-                click.echo(add_quota_accept.stderr)
-            else:
-                click.echo('创建 %s 流量限额接受规则 [完成]' % k)
-            add_auota_drop = subprocess.run(
-                cmd_quota_2.split(), capture_output=True)
-            if add_auota_drop.returncode:
-                click.echo(add_auota_drop.stderr)
-            else:
-                click.echo('创建 %s 流量限额拒绝规则 [完成]' % k)
-            click.echo('初始化完成！')
-
     while True:
         if click.confirm('是否创新的新的防火墙规则？'):
             # TODO 输入端口规则验证
             ports = click.prompt('请输入要开放的端口号，用逗号分隔多个端口，用冒号设置端口段')
-            click.echo('要配置的端口：%s' % ports)
-            for k in quota_chains:
-                click.echo(k)
-            select_chain = click.prompt('请选择流量限额配置')
+            if click.confirm('是否要为端口 %s 设置流量配额？' % ports):
+                input_name = click.prompt('请输入配额链的名字，例如：18000-100G', type=str)
+                chain_name = 'TQ-' + input_name
+                click.echo('将使用 %s 作为限额链的名称' % chain_name)
+                quota = click.prompt('请输入配额总量 Byte', type=int)
+                chain_cmd = 'sudo iptables -N %s' % chain_name
+                add_chain = subprocess.run(chain_cmd.split(), capture_output=True)
+                if add_chain.returncode:
+                    click.echo(add_chain.stderr)
+                    break
+                else:
+                    click.echo('创建 %s 链 [完成]' % chain_name)
+                    cmd_quota_1 = 'sudo iptables -A %s -m quota --quota %s -j ACCEPT' % (chain_name, quota)
+                    cmd_quota_2 = 'sudo iptables -A %s -j DROP' % chain_name
+                    add_quota_accept = subprocess.run(
+                        cmd_quota_1.split(), capture_output=True)
+                    if add_quota_accept.returncode:
+                        click.echo(add_quota_accept.stderr)
+                    else:
+                        click.echo('创建 %s 流量限额接受规则 [完成]' % chain_name)
+                    add_auota_drop = subprocess.run(
+                        cmd_quota_2.split(), capture_output=True)
+                    if add_auota_drop.returncode:
+                        click.echo(add_auota_drop.stderr)
+                    else:
+                        click.echo('创建 %s 流量限额拒绝规则 [完成]' % chain_name)
 
-            cmd_1 = 'sudo iptables -I INPUT -p tcp -m multiport --sports %s -g %s' % (
-                ports, select_chain)
-            cmd_2 = 'sudo iptables -I OUTPUT -p tcp -m multiport --sports %s -g %s' % (
-                ports, select_chain)
+                    cmd_1 = 'sudo iptables -I INPUT -p tcp -m multiport --sports %s -g %s' % (ports, chain_name)
+                    cmd_2 = 'sudo iptables -I OUTPUT -p tcp -m multiport --sports %s -g %s' % (ports, chain_name)
+            
+            else:
+                cmd_1 = 'sudo iptables -I INPUT -p tcp -m multiport --sports %s' % ports
+                cmd_2 = 'sudo iptables -I OUTPUT -p tcp -m multiport --sports %s' % ports
 
-            add_ports_input = subprocess.run(
-                cmd_1.split(), capture_output=True)
+            add_ports_input = subprocess.run(cmd_1.split(), capture_output=True)
             if add_ports_input.returncode:
                 click.echo('创建指定端口的入站规则 [失败]')
                 click.echo(add_ports_input.stderr)
@@ -93,8 +79,7 @@ def addrule():
             else:
                 click.echo('创建指定端口的入站规则 [完成]')
 
-            add_ports_output = subprocess.run(
-                cmd_2.split(), capture_output=True)
+            add_ports_output = subprocess.run(cmd_2.split(), capture_output=True)
             if add_ports_output.returncode:
                 click.echo('创建指定端口的出站规则 [失败]')
                 click.echo(add_ports_output.stderr)
@@ -109,6 +94,26 @@ def addrule():
                 rules = 'sudo iptables -L %s -nv' % c
                 subprocess.run(rules.split())
 
+        else:
+            break
+
+
+@cli.command()
+def delchain():
+    """删除给定的链"""
+    while True:
+        if click.confirm('是否要删除防火墙的链？'):
+            list_chain = 'sudo iptables -L'
+            subprocess.run(list_chain.split())
+            chain = click.prompt('请输入要删除的链名称')
+            while True:
+                del_rule_cmd = 'sudo iptables -D %s 1' % chain
+                del_rule = subprocess.run(del_rule_cmd.split(), capture_output=True)
+                if del_rule.returncode:
+                    del_chain_cmd = 'sudo iptables -X %s' % chain
+                    subprocess.run(del_chain_cmd.split())
+                    click.echo('删除 %s [成功]' % chain)
+                    break
         else:
             break
 
